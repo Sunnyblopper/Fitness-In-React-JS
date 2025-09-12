9974bcd6ffbe5743c5375992f42472abb12a5d06
Hassan Waheed <hassanwaheed040@gmail.com>
2025-09-12 22:10:56 +0500

    Some Changing

--- src/App.jsx
+++ src/App.jsx
@@ -14,9 +14,9 @@ const App = () => {
     <div>
       {/* <FitnessGoal /> */}
 
-      <FitnessExplore />
+      {/* <FitnessExplore /> */}
 
-      {/* <Home /> */}
+      <Home />
     </div>
   )
 }
--- src/Component/Home/home.jsx
+++ src/Component/Home/home.jsx
@@ -3,6 +3,11 @@ import Navbar from "../Navbar/navbar";
 import HeaderCarousel from "../HeaderCarousel/headercarousel";
 import AboutInHome from "../About-In-Home/aboutinhome";
 import FitnessGoal from "../FitnessGoal/fitnessgoal";
+import FitnessFeature from "../FitnessFeature/fitnessfeature";
+import FitnessCourse from "../FitnessCource/fitnesscource";
+import FitnessBlog from "../FitnessBlog/fitnessblog";
+import FitnessExplore from "../FitnessExplore/fitnessexplore";
+import FitnessTeam from "../FitnessTeam/fitnessteam";
 
 const home = () => {
   return (
@@ -11,6 +16,11 @@ const home = () => {
       <HeaderCarousel />
       <AboutInHome />
       <FitnessGoal />
+      <FitnessFeature />
+      <FitnessCourse />
+      <FitnessBlog />
+      <FitnessExplore />
+      <FitnessTeam />
     </div>
   );
 };
